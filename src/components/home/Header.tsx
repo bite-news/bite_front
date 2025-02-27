a6588b0b6a1dec588eb1875d3b74ce9c29f01c3b
Joy Lee <joylee.dev@gmail.com>
2025-02-27 15:52:45 +0900

    fix: 디렉토리 구조 수정 / 경로 업데이트

--- src/components/home/Header.tsx
+++ src/components/home/Header.tsx
@@ -1,13 +1,17 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { IoSearch } from "react-icons/io5";
 
 export default function Header() {
   return (
-    <header className="flex items-center justify-center h-16 bg-white border border-gray-200">
+    <header className="flex items-center justify-between h-16 bg-white border border-gray-200 px-4">
       <Link href="/" className="flex items-center">
         <Image src="/assets/logo.png" alt="logo" width={40} height={20} />
         <span className="text-xl font-bold font-news">Bite News</span>
+      </Link>
+      <Link href="/search">
+        <IoSearch size={28} />
       </Link>
     </header>
   );
---
+++ src/components/home/index.ts
@@ -0,0 +1,3 @@
+export { default as Header } from "./Header";
+export { default as VideoList } from "./VideoList";
+export { default as VideoCard } from "./VideoCard";
