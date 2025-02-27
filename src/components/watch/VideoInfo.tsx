101294ad8f3b81ed0c3c5f0fc0d47de37e46df90
iOdiO89 <117376841+iOdiO89@users.noreply.github.com>
2025-02-27 13:15:00 +0900

    style: 상세페이지 제목 위치 수정

--- src/components/watch/VideoInfo.tsx
+++ src/components/watch/VideoInfo.tsx
@@ -4,8 +4,10 @@ interface VideoInfoProps {
 
 export default function VideoInfo({ info }: VideoInfoProps) {
   return (
-    <div className="absolute bottom-12 px-10">
-      <h2 className="text-white text-xl font-bold">{info.title}</h2>
+    <div className="absolute bottom-8 px-4">
+      <div className="text-white text-xl font-bold line-clamp-1">
+        {info.title}
+      </div>
     </div>
   );
 }
