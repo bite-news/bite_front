8eed9a52ad8eae8473c0395cd12671370df057b6
Joy Lee <joylee.dev@gmail.com>
2025-02-27 11:32:00 +0900

    feat: 무한스크롤 useInfiniteScroll hook 추가

--- src/components/video/VideoList.tsx
+++ src/components/video/VideoList.tsx
@@ -1,16 +1,31 @@
+"use client";
+
 import React from "react";
 import VideoCard from "./VideoCard";
+import { useInfiniteScroll } from "@/lib/hooks/useInfiniteScroll";
 
 interface VideoListProps {
-  videos: Video[];
+  initialVideos: Video[];
 }
 
-export default function VideoList({ videos }: VideoListProps) {
+export default function VideoList({ initialVideos }: VideoListProps) {
+  const { data: videos, isLoading, ref } = useInfiniteScroll(initialVideos);
+
   return (
-    <div className="grid grid-cols-2 gap-4 gap-y-8">
-      {videos.map((video) => (
-        <VideoCard key={video.id} video={video} />
-      ))}
-    </div>
+    <>
+      <div className="grid grid-cols-2 gap-4 gap-y-8">
+        {videos.map((video) => (
+          <VideoCard key={video.id} video={video} />
+        ))}
+      </div>
+
+      <div ref={ref} className="w-full py-4 flex justify-center">
+        {isLoading ? (
+          <div className="loader">로딩 중...</div>
+        ) : (
+          <div className="h-10"></div>
+        )}
+      </div>
+    </>
   );
 }
