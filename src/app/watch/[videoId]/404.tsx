112a985667625bbfca19d067c90706108c3d477c
iOdiO89 <117376841+iOdiO89@users.noreply.github.com>
2025-02-27 11:24:43 +0900

    feat: 상세페이지 404 구현

---
+++ src/app/watch/[videoId]/404.tsx
@@ -0,0 +1,17 @@
+import Header from "@/components/watch/Header";
+import Image from "next/image";
+
+export default function Custom404() {
+  return (
+    <main>
+      <Header />
+      <div className="">
+        <Image
+          src={require("@/assets/images/error-icon.png")}
+          className="h-[200px] w-[200px]"
+          alt="valid video not exits"
+        />
+      </div>
+    </main>
+  );
+}
